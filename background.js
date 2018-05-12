588aa2fcc418383321cb5a24e105c7db4b006abf
Seke Nikola <32386925+SekeNikola@users.noreply.github.com>
2018-05-12 14:30:04 +0200

    Fixed Stop Command

--- background.js
+++ background.js
@@ -1,14 +1,12 @@
 
 
 var audio = document.createElement("audio");
-audio.setAttribute('src', "http://streaming.tdiradio.com:8000/tdiradio.mp3");
-
 
         chrome.commands.onCommand.addListener(function(command) {
-            console.log('Command:', command);
             if(command == "Play"){
+                audio.setAttribute('src', "http://streaming.tdiradio.com:8000/tdiradio.mp3");
                 audio.play();
             }else if(command == "Stop"){
-                audio.pause()
+                audio.setAttribute('src', "");
             }
          });
